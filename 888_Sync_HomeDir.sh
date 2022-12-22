10f0308109671897e4679ce933399855981442cf
David Rihtarsic <david.rihtarsic@gmail.com>
2022-12-22 15:48:10 +0100

    ranger

--- 888_Sync_HomeDir.sh
+++ 888_Sync_HomeDir.sh
@@ -6,5 +6,6 @@ echo "    Make simbolyc links to $HOME directory"
 echo "################################################################################"
 
 tree -dafi --noreport home | xargs -I{} mkdir -p /{}
+find home -type f | xargs -I{} ln -f s ~+/{}
 
 sleep 1
