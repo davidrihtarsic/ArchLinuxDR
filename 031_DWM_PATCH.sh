60adab1140c83eefb99219192bbf88eff21a7c8b
David Rihtarsic <david.rihtarsic@gmail.com>
2022-12-25 22:43:40 +0100

    sdf

--- 031_DWM_PATCH.sh
+++ 031_DWM_PATCH.sh
@@ -5,13 +5,15 @@ echo "##########################################################################
 echo "   Building&PATCHING"
 echo "################################################################################"
 
-PWD=$(pwd)
+#PWD=$(pwd)
+#
+#[[ -d ~/.local/build/dwm ]] && rm -R ~/.local/build/dwm
+#git clone https://git.suckless.org/dwm ~/.local/build/dwm
+#cd ~/.local/build/dwm && sudo make 
+#
+#patch ~/.local/build/dwm/config.h /home/david/Files/GitHub_noSync/ArchLinuxDR/home-david/.config/dwm/davids_dwm.patch
 
-[[ -d ~/.local/build/dwm ]] && rm -R ~/.local/build/dwm
-git clone https://git.suckless.org/dwm ~/.local/build/dwm
-cd ~/.local/build/dwm && sudo make 
-
-patch ~/.local/build/dwm/config.h /home/david/Files/GitHub_noSync/ArchLinuxDR/home-david/.config/dwm/davids_dwm.patch
+./888_Sync_HomeDir.sh
 
 echo -e '\e[32m' #zelena
 echo "################################################################################"
